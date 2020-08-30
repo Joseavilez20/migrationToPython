2f510889a2396a0d5c014531e3401e2fd271ccff
Jose Antonio Aviles Pacheco <joseok56@gmail.com>
2020-08-30 16:17:06 -0500

    writting Script line 106

--- evaluacionalvr2dummi.py
+++ evaluacionalvr2dummi.py
@@ -7,12 +7,29 @@
 #t : 72 rows
 import math
 def evaluacionalvr2(pobinicial2, matrizdatos, m, rr, matrizai, t):
-    for k in range(1,3):
+    for k in range(1,t+1):
         
         for j in range(1,(rr-1)+1):
             
             g1= (pobinicial2[m-1][1+(20*(j-1))-1]) +( pobinicial2[m-1][2+(20*(j-1))-1])*((matrizdatos[k-1][j-1])**(pobinicial2[m-1][10+(20*(j-1))-1]))+( pobinicial2[m-1][3+(20*(j-1))-1])*((matrizdatos[k-1][j-1])**(pobinicial2[m-1][11+(20*(j-1))-1]))+(pobinicial2[m-1][4+(20*(j-1))-1])*((matrizdatos[k-1][j-1])**(pobinicial2[m-1][12+(20*(j-1))-1]))+ (pobinicial2[m-1][5+(20*(j-1))-1])*((matrizdatos[k-1][j-1])**(pobinicial2[m-1][13+(20*(j-1))-1]))+(pobinicial2[m-1][6+(20*(j-1))-1])*((matrizdatos[k-1][j-1])**(pobinicial2[m-1][14+(20*(j-1))-1]))
             
             g2=(abs(pobinicial2[m-1][7+(20*(j-1))-1])+1)**((pobinicial2[m-1][15+(20*(j-1))-1])*(abs(matrizdatos[k-1][j-1]))) +(pobinicial2[m-1][(8+(20*(j-1)))-1])*(math.sin(pobinicial2[m-1][16+(20*(j-1))-1]*(matrizdatos[k-1][j-1])))+(pobinicial2[m-1][(9+(20*(j-1)))-1])*(math.cos((pobinicial2[m-1][(17+(20*(j-1)))-1])*(matrizdatos[k-1][j-1])));
-            print (g2)
-            #print( pobinicial2[m-1][1+(20*(j-1))-1]) 
+
+            if g1 > 0: 
+               g4 = g1
+            else:
+               g4 = 1
+               
+            g = g1 + g2
+
+            g3 = (pobinicial2[m-1][(18+(20*(j-1)))-1])+(pobinicial2[m-1][(19+(20*(j-1)))-1])*(g +(pobinicial2[m-1][(20+(20*(j-1)))-1])*(math.log(g4)))
+
+            z = g/g3
+
+            matrizai[k-1][j-1] = z
+            
+            
+        #endfor
+    #endfor
+    return matrizai
+    
--- readExcel.py
+++ readExcel.py
@@ -1,21 +1,19 @@
 import pandas as pd
-import numpy
+import numpy as np
 from evaluacionalvr2dummi import evaluacionalvr2
 from TestData import testpobinicial2
 archivo = pd.read_excel('MATRIZHV2.xlsx', header=None, skiprows=1, nrows=72)
 matrizdatos= archivo.to_numpy()
-print(testpobinicial2[0][0])
-print(testpobinicial2[0][20])
-
 
 t = archivo.count()[2]  #output 72 
 rr = archivo.columns.size  #output 3
+ 
 
 #matrizai=zeros(t,(rr-1)); #output array dimension 72x2  with zeros
 
-matrizai = numpy.zeros((t, rr-1), dtype=float, order='C')
-matrizai2= numpy.zeros((t, rr-1), dtype=float, order='C')
-matrizai22= numpy.zeros((t, rr-1), dtype=float, order='C')
+matrizai = np.zeros((t, rr-1), dtype=float, order='C')
+matrizai2= np.zeros((t, rr-1), dtype=float, order='C')
+matrizai22= np.zeros((t, rr-1), dtype=float, order='C')
 
 v=(rr-1)*20; #output 40
 
@@ -37,25 +35,75 @@ cont=1
 c4=0
 
 #mejor1=zeros(1,v+1); #output array dimension 1x41 with zeros
-mejor1 = numpy.zeros((1, v+1), dtype=float, order='C')
+mejor1 = np.zeros((1, v+1), dtype=float, order='C')
 
 #mejor1(1,1:v+1)=random('unif',-1,1,1,v+1); #output aleatory number with uniform distribution
-mejor1 = numpy.random.uniform(-1, 1, size=(1, v+1))
+mejor1 = np.random.uniform(-1, 1, size=(1, v+1))
 
 #mejor1(1,v+1)=-inf; #set a value infinite negative
 cd=0
 
+#manipulaMatriz = False
 while cd == 0:
     #pobinicial2=random('unif',-1,1,hh,v); #output aleatory number with uniform distribution, 10x40
-    #pobinicial2 = numpy.random.uniform(-1, 1, size=(hh, v))
+    #pobinicial2 = np.random.uniform(-1, 1, size=(hh, v))
     pobinicial2 = testpobinicial2
     print("BUSQUEDA");
     #R=zeros(hh,1); #array dimension 10x1 with zeros
-    R = numpy.zeros((hh, 1), dtype=float, order='C')
+    R = np.zeros((hh, 1), dtype=float, order='C')
     #for  m=1:hh
     for m in range(1,hh+1):
-       
-        evaluacionalvr2(pobinicial2,matrizdatos,m,rr,matrizai,t);
+        
+        matrizai22 = evaluacionalvr2(pobinicial2,matrizdatos,m,rr,matrizai,t);
+        manipulaMatriz = False
+        for s in range(1,3):
+            #np.insert(arr, 3, [0]*72, axis=1)
+            #stack a new column with zeros data [[0]]*72 2-dimension in the last axis the array
+            if  manipulaMatriz is not True:
+                matrizai22 = np.hstack((matrizai22,[[0]]*72))
+                manipulaMatriz = True
+      
+            matrizai22[s-1][rr-1] = sum(matrizai22[s-1,:t+1])
+            
+            
+        
+        promedioai2 = np.mean(matrizai22[:,rr-1])
+        ybarra2 = np.mean(matrizdatos[:,rr-1])
+        K2 = ybarra2/promedioai2
+        manipulaMatriz = False
+        for w in range(1,t+1):
+            #Note: Add in a fuction
+            if  manipulaMatriz is not True:
+                matrizdatos = np.hstack((matrizdatos,[[0]]*72))
+                manipulaMatriz = True
+                
+            matrizdatos[w-1,rr] = K2*matrizai22[w-1,rr-1]
+            
+            
+        manipulaMatriz = False
+        for zx in range(1,t+1):
+            if  manipulaMatriz is not True:
+                matrizdatos = np.hstack((matrizdatos,[[0]]*72))
+                manipulaMatriz = True
+                
+            matrizdatos[zx-1,rr+1] =  matrizdatos[zx-1,rr-1] - matrizdatos[zx-1,rr]
+            
+        manipulaMatriz = False    
+        for sa in range(1,t+1):
+            if  manipulaMatriz is not True:
+                matrizdatos = np.hstack((matrizdatos,[[0]]*72))
+                manipulaMatriz = True
+                
+            matrizdatos[sa-1,rr+2] = matrizdatos[sa-1,rr] - ybarra2      
+            
+
+        sse = sum(matrizdatos[:,rr+1]**2)
+        ssr = sum(matrizdatos[:,rr+2]**2)
+        sst = sse + ssr
+        
+        R[m-1,0] = ssr / sst
+        print (R[m-1,0])
+        print ("--------------------\n")
     cd = 1      
 
 
